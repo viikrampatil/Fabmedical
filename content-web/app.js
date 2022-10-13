d250bf26656f578dc14accd95361a7af67cdcbb9
viikrampatil <viikrampatil@gmail.com>
2022-10-13 23:41:54 +0000

    Added Application Insights

--- content-web/app.js
+++ content-web/app.js
@@ -4,6 +4,9 @@ const path = require('path');
 const request = require('request');
 
 const app = express();
+const appInsights = require("applicationinsights");
+appInsights.setup("0fc958ec-a4f2-4df7-9f3b-0a82e183bab4");
+appInsights.start();
 
 app.use(express.static(path.join(__dirname, 'dist/content-web')));
 const contentApiUrl = process.env.CONTENT_API_URL || "http://localhost:3001";
